4c5e7cacdc57803a4799be01f683d7e5433478fb
Ethan <ganx0005@e.ntu.edu.sg>
2019-06-28 17:28:23 +0800

    contact

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from comments.forms import CommentForm
 from django.views.generic import ListView,DetailView
 from django.db.models import Q
 def contact(request):
-    return render(request, 'blog/contact.html',{})
+    return render(request, 'blog/contact.html')
 def search(request):
     q = request.GET.get('q')
     error_msg = ''
