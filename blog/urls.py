587701f2778e7cf5a8c553f3a28434c301d0ddd2
Ethan <ganx0005@e.ntu.edu.sg>
2019-07-06 16:27:51 +0800

    contact

--- blog/urls.py
+++ blog/urls.py
@@ -6,9 +6,9 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     url(r'^search/$',views.search,name='search'),
+    url(r'^contact', views.contact,name='contact'),
     url(r'^$', views.IndexView.as_view(), name='index'), #the second parameter must be the function, so use as_view
     url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchievesView.as_view(), name='archives'),
     url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'), #view.category before
-    url(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name='tag'),
-    url(r'^contact/$', views.contact,name='contact')]
+    url(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name='tag'),]
