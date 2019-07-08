f66f3047013f97232d05d399f9e031dc58a9716f
Ethan <ganx0005@e.ntu.edu.sg>
2019-07-08 11:05:23 +0800

    contact

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 
 from django.conf.urls import url
-
+from django.urls import path
 from . import views
 
 app_name = 'blog'
 urlpatterns = [
+    path('contact', views.contact, name='contact'),
     url(r'^search/$',views.search,name='search'),
-    url(r'^contact/$', views.contact,name='contact'),
     url(r'^$', views.IndexView.as_view(), name='index'), #the second parameter must be the function, so use as_view
     url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchievesView.as_view(), name='archives'),
