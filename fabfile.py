bc913cb28230ba14976fb03cbc82541bb9fb7d67
Ethan <ganx0005@e.ntu.edu.sg>
2019-07-17 11:44:19 +0800

    contact

---
+++ fabfile.py
@@ -0,0 +1,26 @@
+from fabric.api import env, run
+from fabric.operations import sudo
+GIT_REPO = "https://github.com/codergan/Myblog.git"
+
+env.user = 'gx'
+env.password = 'lchgx991123'
+
+# 填写你自己的主机对应的域名
+env.hosts = ['120.78.122.131']
+
+# 一般情况下为 22 端口，如果非 22 端口请查看你的主机服务提供商提供的信息
+env.port = '22'
+
+
+def deploy():
+    source_folder = '/home/gx/sites/120.78.122.131/Myblog'
+    run('cd %s && git pull' % source_folder)
+
+    run("""
+        cd {} &&
+        ../env/bin/pip install -r requirements.txt &&
+        ../env/bin/python3 manage.py collectstatic --noinput &&
+        ../env/bin/python3 manage.py migrate
+        """.format(source_folder))
+    sudo('systemctl restart gx.service')
+    sudo('service nginx reload')
